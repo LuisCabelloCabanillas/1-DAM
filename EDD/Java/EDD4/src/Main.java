232a6a329cbd08df487cc6c53cd3ee041aaa8463
Luis Cabello <lcabellocabanilas@safareyes.es>
2025-02-03 14:28:07 +0100

    w

--- EDD/Java/EDD4/src/Main.java
+++ EDD/Java/EDD4/src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PokemonImpl pokemon = new PokemonImpl("Bulbasur",
+                "Planta/Veneno",0001,
+                "Este Pokémon nace con una semilla en el lomo, que\n" +
+                "brota con el paso del tiempo.", 0.7, 6.9, generoPokemon.Masculino ,"Follaje, 40" );
+        System.out.println(pokemon);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("\n" + "Los pokemones que se van a comparar son Charmander y Bulbasur");
+        Map<String, String> debilidades = Map.of(
+                "Fuego", "Planta",
+                "Planta", "Agua",
+                "Agua", "Fuego"
+        );
+
+        Pokemon charmander = new Pokemon("Charmander", "Fuego");
+        Pokemon bulbasaur = new Pokemon("Bulbasaur", "Planta");
+
+        PokedexImpl pokedexCharmander = new PokedexImpl(charmander, debilidades, "Llamarada");
+        PokedexImpl pokedexBulbasaur = new PokedexImpl(bulbasaur, debilidades, "Latigazo");
+
+        Pokemon debil = PokedexImpl.compararPokemons(pokedexCharmander, pokedexBulbasaur);
+
+        if (debil != null) {
+            System.out.println("El Pokémon en desventaja es: " + debil.getNombre());
+        } else {
+            System.out.println("No hay una clara desventaja entre los Pokémon.");
         }
     }
 }
